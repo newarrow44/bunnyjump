bb2606893469048620c41ef6125e425e5c164b42
tanapon <newwenn1@gmail.com>
2019-09-19 12:34:57 +0700

    update2

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,7 @@ var motion = Vector2(0,100)
 export var gravity = 30
 export var SPEED = 300
 export var jump = 700
+var jump_count = 1
 
 func _physics_process(delta):
 	
@@ -19,9 +20,12 @@ func apply_gravity():
 		motion.y = motion.y + gravity
 	
 func jump():
-	if is_on_floor():
+	if jump_count < 1:
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -jump
+			jump_count = jump_count + 1
+	if is_on_floor():
+		jump_count = 0
 	
 func walk():
 	if Input.is_action_pressed("left"):
